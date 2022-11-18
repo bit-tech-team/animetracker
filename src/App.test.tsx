df2fdf9b8913f80b0e8bea84862f4cbea4e7682a
shakarr <risaaflo@gmail.com>
2022-11-18 16:22:36 +0100

    remove App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { mount } from "@cypress/react";
-import App from "./App";
-
-it("renders p", () => {
-  mount(<App />);
-  cy.get("p").contains("Are you looking");
-});
